8458311a60e5f1a1c5f72ccc43ceded6a0f724e5
trevorregister <trevor.register@gmail.com>
2025-03-05 16:01:24 -0500

    feat: login page

---
+++ src/domains/users/pages/LoginPage.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="row">
+      <div class="column">
+        <div class="row justify-center">
+          <form-body header="Login">
+            <form-input label="Email" v-model="email"/>
+            <form-input label="Password" type="password" v-model="password"/>
+            <form-action label="Submit" type="submit" @submit="handleSubmit"/>
+          </form-body>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script setup lang="ts">
+  import client from '../../../shared/api-client'
+  import { ref } from 'vue'
+  import FormBody from '../../../shared/components/FormBody.vue'
+  import FormAction from '../../../shared/components/FormAction.vue'
+  import FormInput from '../../../shared/components/FormInput.vue'
+  
+  const email = ref('')
+  const password = ref('')
+  
+  const handleSubmit = async () => {
+    const user = await client.users.loginEmailPassword({
+      email: email.value,
+      password: password.value
+    })
+  }
+  </script>
+  
+  <style scoped>
+  </style>
+  
--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CreateAccountPage from '../domains/users/pages/CreateAccountPage.vue'
+import LoginPage from '../domains/users/pages/LoginPage.vue'
 //const publicPages = ['/login', '/create-account', '/about', '/test']
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   //routes: [...root, ...teams, ...shifts, ...users],
   routes: [
+    {
+      path: '/login',
+      name: 'login',
+      component: LoginPage
+    },
     {
         path: '/create-account',
         name: 'create-account',

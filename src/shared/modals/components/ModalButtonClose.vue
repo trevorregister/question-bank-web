1516d9bfd3aedbff0b181fb14eccac6ef7171dc3
trevorregister <trevor.register@gmail.com>
2025-03-11 15:34:42 -0400

    feat: confirm modal

---
+++ src/shared/modals/components/ModalButtonClose.vue
@@ -0,0 +1,30 @@
+<template>
+    <q-btn 
+        class="q-ml-md"
+        outline 
+        color="negative" 
+        :label="label" 
+        @click="onClick"
+    />
+</template>
+  
+<script setup lang="ts">
+import { inject } from 'vue'
+import { ModalMethods } from './ModalProvider.vue'
+import { withDefaults } from 'vue'
+
+withDefaults(defineProps<{label: string}>(), {
+    label: 'Close'
+})
+
+const modal = inject<ModalMethods>('$modal')
+
+const onClick = () => {
+    modal.close()
+}
+
+</script>
+
+<style lang="scss">
+
+</style>
--- src/shared/modals/components/ModalProvider.vue
+++ src/shared/modals/components/ModalProvider.vue
@@ -2,21 +2,24 @@
     <div>
         <slot></slot>
     </div>
-    <div v-if="modalComponent">
+    <q-dialog v-model="showModal">
         <component v-bind="modalProps" ref="modal" :is="modalComponent" />
-    </div>
+    </q-dialog>
 </template>
 
 <script setup lang="ts">
-import { ComponentPublicInstance, ref, provide, markRaw } from 'vue'
+import { ComponentPublicInstance, ref, provide, markRaw, computed } from 'vue'
 
 const modalComponent = ref<ComponentPublicInstance>(null)
+const showModal = ref<boolean>(false)
 const modalResolve = ref(null)
 const modalProps = ref(null)
 
+
 const show = async (component: ComponentPublicInstance, props = {}) => {
+    showModal.value = true
     const previousElement = document.activeElement as HTMLElement
-    const result = await new Promise(resolve => {
+    const result = await new Promise((resolve, reject) => {
         modalResolve.value = resolve
         modalProps.value = props
         modalComponent.value = markRaw(component)
@@ -27,18 +30,21 @@ const show = async (component: ComponentPublicInstance, props = {}) => {
     return result
 }
 const close = () => {
+    showModal.value = false
     modalComponent.value = null
     modalProps.value = {}
     modalResolve.value = modalResolve.value({status: 'closed'})
     modalResolve.value = null
 }
-const ok = (data: any) => {
+const ok = (data = {}) => {
+    showModal.value = false
     modalComponent.value = null
     modalProps.value = {}
     modalResolve.value = modalResolve.value({status: 'ok', data})
     modalResolve.value = null
 }
 const status = (status: string, data: any) => {
+    showModal.value = false
     modalComponent.value = null
     modalProps.value = {}
     modalResolve.value = modalResolve.value({ status, data })
@@ -47,7 +53,7 @@ const status = (status: string, data: any) => {
 export interface ModalMethods {
     show: (component,  props?) => Promise<any>
     close: () => void
-    ok: (data: any) => void
+    ok: (data?: any) => void
     status: (status: string, data: any) => void
 }
 const methods: ModalMethods = {

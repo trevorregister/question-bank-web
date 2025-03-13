e130b6467e4c3149a9b9a34b3fcbde969e70d4ee
Trevor Register <79274467+trevorregister@users.noreply.github.com>
2025-03-13 10:55:06 -0400

    feat: flash provider first commit

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,13 @@
   <q-layout view="hHh lpR fFf" class="q-pa-lg">
     <AppHeader/>
     <q-page-container>
-      <ModalProvider>
-        <div class="flex justify-center">
-          <router-view/>
-        </div>
-      </ModalProvider>
+      <FlashProvider>
+        <ModalProvider>
+          <div class="flex justify-center">
+            <router-view/>
+          </div>
+        </ModalProvider>
+      </FlashProvider>
     </q-page-container>
     <AppFooter/>
   </q-layout>
@@ -17,6 +19,7 @@ import { RouterView } from 'vue-router'
 import AppHeader from './shared/components/AppHeader.vue'
 import AppFooter from './shared/components/AppFooter.vue'
 import ModalProvider from './shared/modals/components/ModalProvider.vue'
+import FlashProvider from './shared/FlashProvider.vue'
 
 </script>
 
---
+++ src/shared/Flash.vue
@@ -0,0 +1,46 @@
+<template>
+    <q-banner
+        dense
+        rounded
+        class="flash-banner q-pa-sm q-mb-sm text-body2"
+        :color="flashType"
+    >
+        {{ message }}
+    </q-banner>
+</template>
+
+<script setup lang="ts">
+import { ComponentPublicInstance, ref, provide, markRaw, computed } from 'vue'
+
+const props = defineProps<{message: string, type: string}>()
+
+const flashType = (): string => {
+    switch(props.type){
+        case 'success':
+            return 'positive'
+        case 'warning':
+            return 'warning'
+        case 'error':
+            return 'negative'
+    }
+}
+
+/* export interface Flash = {
+
+}
+const methods: Flash = {
+    ok: ok, 
+    status: status, 
+    close: close, 
+    show: show
+}
+provide('$flash', methods) */
+
+</script>
+
+<style lang="scss">
+.flash-banner {
+    display: block;
+}
+
+</style>

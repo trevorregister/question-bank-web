58865dd3244037e00a9d91b2ca3b4e2456c3608d
trevorregister <trevor.register@gmail.com>
2025-03-14 16:39:13 -0400

    feat: builder seed

--- src/domains/users/e2e/login.e2e.ts
+++ src/domains/users/e2e/login.e2e.ts
@@ -1,13 +1,17 @@
 import { test, expect } from "../../../e2e"
 import builder from "../../../db/builder"
 
-test("existing user can login", async ({ page }) => {
+test("existing user can login", async ({ page, seed }) => {
+  const teacher = builder.user.teacher({ email: "asdf@asdfasdfasdf.asdfasdf" })
+  const bank = builder.bank({ owner: teacher._id })
+  await seed(builder.data)
   await page.goto("/login")
   await expect(page.getByText("Login")).toBeVisible()
-  await page.getByLabel("Email").fill("teacher1@asdf.com")
+  await page.getByLabel("Email").fill(teacher.email)
   await page.getByLabel("Password").fill("asdf")
   await page.getByRole("button", { name: "Submit" }).click()
   await expect(
     page.getByRole("button", { name: "Create New Bank" }),
   ).toBeVisible()
+  await expect(page.getByText(bank.name)).toBeVisible()
 })
--- src/e2e.ts
+++ src/e2e.ts
@@ -1,7 +1,10 @@
 import { Page, test as base } from "@playwright/test"
+import dotenv from "dotenv"
+dotenv.config()
 
 type E2EFixtures = {
   login(email: string, password: string, page?: Page): Promise<void>
+  seed(data: any): Promise<void>
   logout(page?: Page): Promise<void>
 }
 
@@ -14,6 +17,16 @@ export const test = base.extend<E2EFixtures>({
           data: { email, password },
         },
       )
+    })
+  },
+  seed: async ({ page }, use) => {
+    await use(async (data: any, _page: Page = page) => {
+      await _page.request.post("http://localhost:3000/api/test/seed", {
+        data: data,
+        headers: {
+          Authorization: `Bearer ${process.env.VITE_SEED_TOKEN}`,
+        },
+      })
     })
   },
 })
